be6a95980426751cc19b9a447b236cbce5cfef2f
Wesley Bowman <wesley.bowman23@gmail.com>
2013-04-02 15:06:47 -0300

    added description to the program

--- shocksInBurgersEquation.py
+++ shocksInBurgersEquation.py
@@ -1,7 +1,25 @@
 '''
+    shocksInBurgersEquation lets the user choose an initial function and find 
+    the shocks of that function using the equal area rule. 
+    Copyright (C) 2013 Wesley A. Bowman
 
+    This program is free software; you can redistribute it and/or modify 
+    it under the terms of the GNU General Public License as published by 
+    the Free Software Foundation; either version 2 of the License, or 
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful, 
+    but WITHOUT ANY WARRANTY; without even the implied warranty of 
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License 
+    along with this program; if not, write to the 
+    Free Software Foundation, Inc., 59 Temple Place, 
+    Suite 330, Boston, MA 02111-1307 USA
 
 '''
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
